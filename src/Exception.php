1c0029969f61367df1ea4011f0a51c58f980917b
xltxlm <xltxlm@qq.com>
2018-11-01 15:23:30 +0800

    初始化项目

---
+++ src/Exception.php
@@ -0,0 +1,13 @@
+<?php
+namespace xltxlm\exception;
+
+
+/**
+ * 最基础的异常类,其他的异常均写在自己的业务里面;
+*/
+class Exception extends Exception\Exception_implements
+{
+
+
+
+}
---
+++ src/Exception/Exception_implements.php
@@ -0,0 +1,12 @@
+<?php
+namespace xltxlm\exception\Exception;
+
+/**
+ * 最基础的异常类,其他的异常均写在自己的业务里面;
+*/
+abstract class Exception_implements
+{
+
+
+
+}
